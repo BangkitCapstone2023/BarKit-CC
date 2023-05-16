30828759788986b8f92be8be22029f3f8bafab83
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-05-17 01:32:40 +0800

    add register data to firestore

--- app/controllers/authHandler.js
+++ app/controllers/authHandler.js
@@ -1,25 +1,24 @@
 const admin = require('firebase-admin');
 const firebase = require('firebase/compat/app');
 require('firebase/compat/auth');
+require('firebase/compat/firestore');
 
 // Inisialisasi Firebase client-side app
 const clientConfig = require('../config/firebaseClientConfig.json');
 firebase.initializeApp(clientConfig);
 
-// Login user using Firebase Admin SDK
+// Login user menggunakan Firebase Admin SDK
 async function loginUser(email, password) {
   try {
     const userCredential = await admin.auth().getUserByEmail(email);
     const { uid } = userCredential;
 
-    // Use the Firebase JavaScript SDK for client-side authentication
+    // Gunakan Firebase JavaScript SDK untuk otentikasi di sisi klien
     const userAuth = await firebase
       .auth()
       .signInWithEmailAndPassword(email, password);
 
-    // console.log(userAuth);
-
-    // Generate a custom token using the Firebase Admin SDK
+    // Generate custom token menggunakan Firebase Admin SDK
     const token = await admin.auth().createCustomToken(uid);
     return token;
   } catch (error) {
@@ -28,21 +27,52 @@ async function loginUser(email, password) {
   }
 }
 
-// Create user using Firebase Admin SDK
-async function createUser(email, password) {
+// Create user menggunakan Firebase Admin SDK
+async function createUser(
+  email,
+  password,
+  username,
+  fullName,
+  address = '',
+  phone = '',
+  gender = ''
+) {
+  const db = admin.firestore();
+
   try {
-    const user = await admin.auth().createUser({
+    const userRecord = await admin.auth().createUser({
       email,
       password,
       emailVerified: false,
       disabled: false,
     });
-    return user;
+
+    // Simpan data tambahan ke Firestore jika tidak kosong
+    const userDocRef = db.collection('users').doc(userRecord.uid);
+    const userData = {
+      id: userRecord.uid,
+      email,
+      password,
+      username,
+      fullName,
+      phone: phone || '',
+      address: address || '',
+      gender: gender || 'male',
+    };
+
+    await userDocRef.set(userData);
+
+    return userRecord;
   } catch (error) {
     console.error('Error creating user:', error);
     throw error;
   }
 }
+
+module.exports = {
+  loginUser,
+  createUser,
+};
 
 module.exports = {
   loginUser,
--- app/routes/authRoute.js
+++ app/routes/authRoute.js
@@ -28,17 +28,34 @@ router.post('/login', async (req, res) => {
 
 // Register route
 router.post('/register', async (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
 
   const user = {
     email: req.body.email,
     password: req.body.password,
+    username: req.body.username,
+    fullName: req.body.fullName,
+    address: req.body.address,
+    phone: req.body.phone,
+    gender: req.body.gender,
   };
 
   try {
+    // Validasi data yang wajib diisi (email, password, username, fullName)
+    if (!user.email || !user.password || !user.username || !user.fullName) {
+      return res.status(400).json({
+        error: 'Email, password, username, and fullName are required',
+      });
+    }
+
     const userResponse = await authHandler.createUser(
       user.email,
-      user.password
+      user.password,
+      user.username,
+      user.fullName,
+      user.address,
+      user.phone,
+      user.gender
     );
     res.json(userResponse);
   } catch (error) {

e0bd92a579c7aa4347966994565bda17061096f6
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-05-22 23:26:13 +0800

    update auth handler

--- app/controllers/authHandler.js
+++ app/controllers/authHandler.js
@@ -2,6 +2,7 @@ import admin from 'firebase-admin';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import { badResponse, successResponse } from '../utils/response.js';
+import { db } from '../config/configFirebase.js';
 
 // Inisialisasi Firebase client-side app
 import { fileURLToPath } from 'url';
@@ -18,10 +19,8 @@ const clientConfigPath = join(
 const clientConfig = JSON.parse(readFileSync(clientConfigPath, 'utf8'));
 firebase.initializeApp(clientConfig);
 
-import { db } from '../config/configFirebase.js';
-
-// Login user menggunakan Firebase Admin SDK
-async function login(req, res) {
+// Login Renters
+const login = async (req, res) => {
   const user = {
     email: req.body.email,
     password: req.body.password,
@@ -36,7 +35,6 @@ async function login(req, res) {
     };
 
     const response = successResponse(200, 'User Success Login', userLoginData);
-
     res.status(200).json(response);
   } catch (error) {
     let errorMessage = '';
@@ -45,17 +43,41 @@ async function login(req, res) {
       errorMessage = 'Password yang dimasukkan salah';
     } else if (error.code === 'auth/invalid-email') {
       errorMessage = 'Email pengguna tidak ditemukan';
+    } else if (error.code === 'auth/user-not-found') {
+      errorMessage = 'User tidak ditemukan';
     } else {
       errorMessage = `Error logging in user: ${error}`;
     }
-
-    const response = badResponse(401, errorMessage);
+    const response = badResponse(401, errorMessage, error.message);
     res.status(401).json(response);
   }
-}
+};
 
-// Create user using Firebase Admin SDK
-async function register(req, res) {
+const loginUser = async (email, password) => {
+  try {
+    const timestamp = admin.firestore.Timestamp.now(); // Mendapatkan timestamp saat login
+
+    // Otentikasi di sisi klien
+    const userCredential = await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password);
+    const { uid } = userCredential.user;
+
+    // Generate custom token
+    const token = await admin.auth().createCustomToken(uid);
+
+    return {
+      token: token,
+      loginTime: timestamp.toDate(),
+    };
+  } catch (error) {
+    console.error('Error logging in user:', error);
+    throw error;
+  }
+};
+
+// Create Renters
+const register = async (req, res) => {
   const user = {
     email: req.body.email,
     password: req.body.password,
@@ -85,49 +107,12 @@ async function register(req, res) {
     res.status(201).json(response);
     console.log(`Success Create User ${user.username}`);
   } catch (error) {
-    let errorMessage = '';
-
-    if (
-      error.message === 'Email, password, username, and fullName are required'
-    ) {
-      errorMessage = error.message;
-    } else if (error.message.startsWith('Username')) {
-      errorMessage = error.message;
-    } else {
-      errorMessage = error;
-    }
-
-    const response = badResponse(400, errorMessage);
+    const response = badResponse(400, error, error);
     res.json(response);
   }
-}
+};
 
-// Login user menggunakan Firebase Admin SDK
-async function loginUser(email, password) {
-  try {
-    const timestamp = admin.firestore.Timestamp.now(); // Mendapatkan timestamp saat login
-
-    // Gunakan Firebase JavaScript SDK untuk otentikasi di sisi klien
-    const userCredential = await firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password);
-    const { uid } = userCredential.user;
-
-    // Generate custom token menggunakan Firebase Admin SDK
-    const token = await admin.auth().createCustomToken(uid);
-
-    return {
-      token: token,
-      loginTime: timestamp.toDate(), // Mengembalikan waktu login dalam bentuk objek Date
-    };
-  } catch (error) {
-    console.error('Error logging in user:', error);
-    throw error;
-  }
-}
-
-// Create user using Firebase Admin SDK
-async function createUser(
+const createUser = async (
   email,
   password,
   username,
@@ -136,11 +121,24 @@ async function createUser(
   phone = '',
   gender = 'male',
   isLessor = false
-) {
+) => {
   try {
     // Validating required fields
     if (!email || !password || !username || !fullName) {
-      throw new Error('Email, password, username, and fullName are required');
+      let errorMessage = '';
+      if (!email) {
+        errorMessage += 'Email is required. ';
+      }
+      if (!password) {
+        errorMessage += 'Password is required. ';
+      }
+      if (!username) {
+        errorMessage += 'Username is required. ';
+      }
+      if (!fullName) {
+        errorMessage += 'Full Name is required. ';
+      }
+      throw new Error(errorMessage.trim());
     }
 
     // Validating username uniqueness
@@ -149,14 +147,12 @@ async function createUser(
       .where('username', '==', username)
       .get();
     if (!usernameSnapshot.empty) {
-      throw new Error(`Username "${username}" is already taken`);
+      throw new Error(`Username '${username}' is already taken`);
     }
 
     const userRecord = await admin.auth().createUser({
       email,
       password,
-      emailVerified: false,
-      disabled: false,
     });
 
     // Save additional data to Firestore if not empty
@@ -173,13 +169,15 @@ async function createUser(
       isLessor,
     };
 
+    const responseData = { ...userData, userRecord };
+
     await userDocRef.set(userData);
     console.log(`Success Store Renter to firestore ${username}`);
-    return userRecord;
+    return responseData;
   } catch (error) {
-    console.error('Error creating user:', error);
-    throw error;
+    console.error('Error creating user:', error.message);
+    throw error.message;
   }
-}
+};
 
 export { login, register };

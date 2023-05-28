bcfa020e6c614fd66829c555e415bbe422563c5b
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-05-28 09:55:13 +0800

    update

--- app/models/model.js
+++ app/models/model.js
@@ -17,6 +17,9 @@ const modelPath = join(__dirname, 'model', 'model.json');
 // List of categories
 const categories = [
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 4bbf4c5 (update)
   'camera',
   'lcd',
   'matras',
@@ -25,6 +28,7 @@ const categories = [
   'speaker',
   'tas',
   'tenda',
+<<<<<<< HEAD
 ];
 
 const predictionModel = async (file, category) => {
@@ -79,72 +83,51 @@ const predictionModel = async (file, category) => {
   'SPEAKER',
   'TAS',
   'TENDA',
+=======
+>>>>>>> 4bbf4c5 (update)
 ];
 
-const predictionModel = async (req, res) => {
+const predictionModel = async (file, category) => {
   try {
-    upload.single('image')(req, res, async (err) => {
-      if (err) {
-        console.error('Error while uploading file:', err);
-        const response = badResponse(
-          500,
-          'An error occurred while uploading the file'
-        );
-        return res.status(500).json(response);
-      }
+    // Proses gambar yang diunggah
+    const image = await loadImage(file.buffer);
+    const canvas = createCanvas(150, 150);
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(image, 0, 0, 150, 150);
 
-      const file = req.file;
+    // Convert gambar canvas menjadi buffer
+    const buffer = canvas.toBuffer('image/jpeg');
 
-      // Continue with file processing
-      const image = await loadImage(file.buffer);
-      const canvas = createCanvas(150, 150);
-      const ctx = canvas.getContext('2d');
-      ctx.drawImage(image, 0, 0, 150, 150);
+    // Load model dari file JSON
+    const modelData = readFileSync(modelPath, 'utf-8');
+    const model = await tf.loadLayersModel(`file://${modelPath}`);
 
-      // Convert the canvas image to a buffer
-      const buffer = canvas.toBuffer('image/jpeg');
+    // Convert buffer gambar menjadi tensor
+    const input = tf.node
+      .decodeImage(buffer)
+      .resizeNearestNeighbor([150, 150])
+      .expandDims()
+      .toFloat()
+      .div(255);
 
-      // Load the model from the JSON file
-      const modelData = readFileSync(modelPath, 'utf-8');
-      const model = await tf.loadLayersModel(`file://${modelPath}`);
+    // Lakukan prediksi menggunakan model
+    const predictions = model.predict(input);
 
-      // Convert the image buffer to a tensor
-      const input = tf.node
-        .decodeImage(buffer)
-        .resizeNearestNeighbor([150, 150])
-        .expandDims()
-        .toFloat()
-        .div(255);
+    // Dapatkan kelas yang diprediksi
+    const predictedClass = predictions.argMax(1).dataSync()[0];
+    const predictedCategory = categories[predictedClass];
 
-      // Make predictions using the model
-      const predictions = model.predict(input);
-
-      // Get the predicted class
-      const predictedClass = predictions.argMax(1).dataSync()[0];
-      const predictedCategory = categories[predictedClass];
-
-      // Display the prediction result
-      console.log('DETECTION RESULT:');
-      console.log();
-
-      if (predictedCategory === req.body.category) {
-        console.log(`Successfully uploaded ${predictedCategory}`);
-        return res.status(200).json(predictedCategory);
-      } else {
-        console.log(
-          `Failed, the image is ${predictedCategory}, not ${req.body.category}`
-        );
-        return res
-          .status(404)
-          .send(
-            `Failed, the image is ${predictedCategory}, not ${req.body.category}`
-          );
-      }
-
-      // Delete the file after processing is complete
-      // fs.unlinkSync(file.path);
-    });
+    // Bandingkan kategori prediksi dengan kategori yang diberikan
+    if (predictedCategory === category) {
+      // Prediksi sesuai dengan kategori yang diberikan
+      return { success: true, predictedCategory };
+    } else {
+      // Prediksi tidak sesuai dengan kategori yang diberikan
+      const errorMessage = `Failed, the image is ${predictedCategory}, not ${category}`;
+      return { success: false, errorMessage };
+    }
   } catch (error) {
+<<<<<<< HEAD
     console.error('Error while processing the image:', error);
 
     const response = badResponse(
@@ -154,6 +137,11 @@ const predictionModel = async (req, res) => {
     );
     return res.status(500).json(response);
 >>>>>>> 14ea090 (update work)
+=======
+    // Handle error saat memproses prediksi
+    // ...
+    return { success: false, errorMessage: error.message };
+>>>>>>> 4bbf4c5 (update)
   }
 };
 

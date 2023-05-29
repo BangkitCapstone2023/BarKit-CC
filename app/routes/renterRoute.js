f72cf5be0684a70bb1303cfc8d810fe9aab21382
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-05-30 03:01:32 +0800

    add CRUD cart api

--- app/routes/renterRoute.js
+++ app/routes/renterRoute.js
@@ -17,7 +17,15 @@ import {
   updateOrder,
 } from '../controllers/renterHandler.js';
 
+import {
+  addProductToCart,
+  getCartProductsByRenter,
+  updateCartProductQuantity,
+  deleteCartProduct,
+} from '../controllers/productHandler.js';
+
 // Rute-rute yang tidak membutuhkan otentikasi
+// Explore Product Routes
 router.get('/', getDashboardData);
 router.get('/search', searchProduct);
 router.get('/category', getAllCategories);
@@ -27,11 +35,20 @@ router.get('/products/:productId', getProductById);
 
 router.use(authMiddleware);
 // Rute-rute yang membutuhkan otentikasi
+// Profile Renter Routes
 router.get('/:username/profile', getUserProfile);
 router.put('/:username/profile', updateProfile);
 router.delete('/renters/:renterId/profile', deleteRenterById);
-router.get('/:username/orders', getOrdersByRenter);
+
+// Cart Routes
+router.post('/:username/products/:productId/carts', addProductToCart);
+router.get('/:username/carts', getCartProductsByRenter);
+router.put('/:username/carts/:productId', updateCartProductQuantity);
+router.delete('/:username/carts/:productId', deleteCartProduct);
+
+// Order Renter Routes
 router.post('/:username/orders/:productId', createOrder);
+router.get('/:username/orders', getOrdersByRenter);
 router.get('/:username/orders/:orderId', getDetailOrdersByRenter);
 router.put('/:username/orders/:orderId', updateOrder);
 

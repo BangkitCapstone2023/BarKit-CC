58cd18ed0bf1875b523b23bd2db8bd596b0950c8
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-06-08 18:36:36 +0800

    update file name

--- app/middlewares/authentication.middlewares.js
+++ app/middlewares/authentication.middlewares.js
@@ -12,14 +12,15 @@ const authMiddleware = async (req, res, next) => {
     const token = authorization.split('Bearer ')[1];
 
     // Periksa apakah renter sudah logout
-    const tokenSnapshoot = db.collection('tokens').doc(token);
-    const tokenRef = await tokenSnapshoot.get();
+    const tokenSnapshot = db.collection('tokens').doc(token);
+    const tokenRef = await tokenSnapshot.get();
     if (tokenRef.exists && tokenRef.data().invalid) {
       throw new Error('Unauthorized');
     }
 
     const decodedToken = await admin.auth().verifyIdToken(token);
     req.user = decodedToken;
+    console.log(decodedToken);
 
     const docRef = db.collection('tokens').doc(token);
     const doc = await docRef.get();
@@ -37,8 +38,8 @@ const authMiddleware = async (req, res, next) => {
 const adminMiddleware = async (req, res, next) => {
   const { authorization } = req.headers;
   try {
-    if (!authorization) {
-      res.status(403).json({ status: 403, message: 'Forbidden' });
+    if (!authorization || !authorization.startsWith('Bearer ')) {
+      res.status(401).json({ status: 401, message: 'Unauthorized' });
       return;
     }
     const token = authorization.split('Bearer ')[1];

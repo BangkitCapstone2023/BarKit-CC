a398703e3d5580a6e3769258da1ddb0a38d3b016
dianayurahmawati <121020873+dianayurahmawati@users.noreply.github.com>
2023-06-16 16:11:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import renterRouter from './app/routes/renter.routes.js';
 
 const app = express();
 const port = 8080;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
